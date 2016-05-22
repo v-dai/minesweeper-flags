09a9eeb8bad7ffa670ee72fc21b20c547c106bbd
Vivian Dai <viviandai_12@hotmail.com>
2016-05-22 09:20:55 -0600

    home page buttons, new activity files

---
+++ app/src/main/java/com/vdai/minesweeperflags/HowToPlayActivity.java
@@ -0,0 +1,15 @@
+package com.vdai.minesweeperflags;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HowToPlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to_play);
+        setTitle("How to play");
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
--- app/src/main/java/com/vdai/minesweeperflags/MainActivity.java
+++ app/src/main/java/com/vdai/minesweeperflags/MainActivity.java
@@ -1,7 +1,9 @@
 package com.vdai.minesweeperflags;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playWithAI(View view) {
+
+    }
+
+    public void playWithFriend(View view) {
+
+    }
+
+    public void settings(View view) {
+
+    }
+
+    public void howToPlay(View view) {
+        Intent intent = new Intent(this, HowToPlayActivity.class);
+        startActivity(intent);
     }
 }
